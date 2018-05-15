6cc98ae7bc4b8d4534446c1c535d89495ebb8f6d
MariannaMilovanova <mmilovanova@kse.org.ua>
2018-05-15 22:08:15 +0300

    files

--- src/components/ScheduleForm/subData.js
+++ src/components/ScheduleForm/subData.js
@@ -1,14 +1,16 @@
 export const FIELDS = {
   wakeUp: {
-    label: 'Enter wake up time'
+    label: 'Wake up'
   },
   timeAtTransportStart: {
-    label: 'Enter time spend in transport'
+    label: 'Transport'
   },
   timeAtTransportEnd: {
-    label: 'Enter time spend in transport'
+    label: 'Transport'
   },
-  coffeeStart: {},
+  coffeeStart: {
+    label: 'Coffee'
+  },
   startingWorkFlow: {},
   standUpMeetingStart: {},
   standUpMeetingEnd: {},
@@ -24,432 +26,387 @@ export const FIELDS = {
 
 export const meansOfTransport = ['car', 'bicycle', 'public transport'];
 
-export const timeIntervals = [
-  {
-    label: '0 min',
-    value: 0
-  },
-  {
-    label: '5 min',
-    value: 0
-  },
-  {
-    label: '10 min',
-    value: 0
-  },
-  {
-    label: '15 min',
-    value: 0
-  },
-  {
-    label: '30 min',
-    value: 0
-  },
-  {
-    label: '45 min',
-    value: 0
-  },
-  {
-    label: '1 hour',
-    value: 0
-  },
-  {
-    label: '1.5 hour',
-    value: 0
-  },
-  {
-    label: '2 hours',
-    value: 0
-  },
-  {
-    label: '2.5 hours',
-    value: 0
-  },
-  {
-    label: '3 hours',
-    value: 0
-  }
-];
-
 export const timeArray = [
   {
     label: '05:00',
-    value: 5
-  },
-  {
-    label: '05:15',
-    value: 5.25
-  },
-  {
-    label: '05:30',
-    value: 5.5
-  },
-  {
-    label: '04:45',
-    value: 4.75
-  },
-  {
-    label: '06:00',
-    value: 6
-  },
-  {
-    label: '06:15',
-    value: 6.25
-  },
-  {
-    label: '06:30',
-    value: 6.5
-  },
-  {
-    label: '06:45',
-    value: 6.75
-  },
-  {
-    label: '07:00',
-    value: 7
-  },
-  {
-    label: '07:15',
-    value: 7.25
-  },
-  {
-    label: '07:30',
-    value: 7.5
-  },
-  {
-    label: '07:45',
-    value: 7.75
-  },
-  {
-    label: '08:00',
-    value: 8
-  },
-  {
-    label: '08:15',
-    value: 8.25
-  },
-  {
-    label: '08:30',
-    value: 8.5
-  },
-  {
-    label: '08:45',
-    value: 8.75
-  },
-  {
-    label: '09:00',
-    value: 9
-  },
-  {
-    label: '09:15',
-    value: 9.25
-  },
-  {
-    label: '09:30',
-    value: 9.5
-  },
-  {
-    label: '09:45',
-    value: 9.75
-  },
-  {
-    label: '10:00',
-    value: 10
-  },
-  {
-    label: '10:15',
-    value: 10.25
-  },
-  {
-    label: '10:30',
-    value: 10.5
-  },
-  {
-    label: '10:45',
-    value: 10.75
-  },
-  {
-    label: '11:00',
-    value: 11
-  },
-  {
-    label: '11:15',
-    value: 11.25
-  },
-  {
-    label: '11:30',
-    value: 11.5
-  },
-  {
-    label: '11:45',
-    value: 11.75
-  },
-  {
-    label: '12:00',
-    value: 12.5
-  },
-  {
-    label: '12:15',
-    value: 12.25
-  },
-  {
-    label: '12:30',
-    value: 12.5
-  },
-  {
-    label: '12:45',
-    value: 12.75
-  },
-  {
-    label: '13:00',
-    value: 13
-  },
-  {
-    label: '13:15',
-    value: 13.25
-  },
-  {
-    label: '13:30',
-    value: 13.5
-  },
-  {
-    label: '13:45',
-    value: 13.75
-  },
-  {
-    label: '14:00',
-    value: 14
-  },
-  {
-    label: '14:15',
-    value: 14.25
-  },
-  {
-    label: '14:30',
-    value: 14.5
-  },
-  {
-    label: '14:45',
-    value: 14.75
-  },
-  {
-    label: '15:00',
-    value: 15
-  },
-  {
-    label: '15:15',
-    value: 15.25
-  },
-  {
-    label: '15:30',
-    value: 15.5
-  },
-  {
-    label: '15:45',
-    value: 15.75
-  },
-  {
-    label: '16:00',
-    value: 16
-  },
-  {
-    label: '16:15',
-    value: 16.25
-  },
-  {
-    label: '16:30',
-    value: 16.5
-  },
-  {
-    label: '16:45',
-    value: 16.75
-  },
-  {
-    label: '17:00',
-    value: 17
-  },
-  {
-    label: '17:15',
-    value: 17.25
-  },
-  {
-    label: '17:30',
-    value: 17.5
-  },
-  {
-    label: '17:45',
-    value: 17.75
-  },
-  {
-    label: '18:00',
-    value: 18
-  },
-  {
-    label: '18:15',
-    value: 18.25
-  },
-  {
-    label: '18:30',
-    value: 18.5
-  },
-  {
-    label: '19:00',
-    value: 19
-  },
-  {
-    label: '19:15',
-    value: 19.25
-  },
-  {
-    label: '19:30',
-    value: 19.5
-  },
-  {
-    label: '19:45',
-    value: 19.75
-  },
-  {
-    label: '20:00',
-    value: 20
-  },
-  {
-    label: '20:15',
-    value: 20.25
-  },
-  {
-    label: '20:30',
-    value: 20.5
-  },
-  {
-    label: '20:45',
-    value: 20.75
-  },
-  {
-    label: '21:00',
-    value: 21
-  },
-  {
-    label: '21:15',
-    value: 21.25
-  },
-  {
-    label: '21:30',
-    value: 21.5
-  },
-  {
-    label: '21:45',
-    value: 21.75
-  },
-  {
-    label: '22:00',
-    value: 22
-  },
-  {
-    label: '22:15',
-    value: 22.25
-  },
-  {
-    label: '22:30',
-    value: 22.5
-  },
-  {
-    label: '22:45',
-    value: 22.75
-  },
-  {
-    label: '23:00',
-    value: 23
-  },
-  {
-    label: '23:15',
-    value: 23.25
-  },
-  {
-    label: '23:30',
-    value: 23.5
-  },
-  {
-    label: '23:45',
-    value: 23.75
-  },
-  {
-    label: '00:00',
     value: 0
   },
   {
-    label: '00:15',
-    value: 0.25
-  },
-  {
-    label: '00:30',
-    value: 0.5
-  },
-  {
-    label: '00:45',
-    value: 0.75
-  },
-  {
-    label: '01:00',
+    label: '05:15',
     value: 1
   },
   {
-    label: '01:15',
-    value: 1.25
-  },
-  {
-    label: '01:30',
-    value: 1.5
-  },
-  {
-    label: '01:45',
-    value: 1.75
-  },
-  {
-    label: '02:00',
+    label: '05:30',
     value: 2
   },
   {
-    label: '02:15',
-    value: 2.25
-  },
-  {
-    label: '02:30',
-    value: 2.5
-  },
-  {
-    label: '02:45',
-    value: 2.75
-  },
-  {
-    label: '03:00',
+    label: '04:45',
     value: 3
   },
   {
-    label: '03:15',
-    value: 3.25
-  },
-  {
-    label: '03:30',
-    value: 3.5
-  },
-  {
-    label: '03:45',
-    value: 3.75
-  },
-  {
-    label: '04:00',
+    label: '06:00',
     value: 4
   },
   {
+    label: '06:15',
+    value: 5
+  },
+  {
+    label: '06:30',
+    value: 6
+  },
+  {
+    label: '06:45',
+    value: 7
+  },
+  {
+    label: '07:00',
+    value: 8
+  },
+  {
+    label: '07:15',
+    value: 9
+  },
+  {
+    label: '07:30',
+    value: 10
+  },
+  {
+    label: '07:45',
+    value: 11
+  },
+  {
+    label: '08:00',
+    value: 12
+  },
+  {
+    label: '08:15',
+    value: 13
+  },
+  {
+    label: '08:30',
+    value: 14
+  },
+  {
+    label: '08:45',
+    value: 15
+  },
+  {
+    label: '09:00',
+    value: 16
+  },
+  {
+    label: '09:15',
+    value: 17
+  },
+  {
+    label: '09:30',
+    value: 18
+  },
+  {
+    label: '09:45',
+    value: 19
+  },
+  {
+    label: '10:00',
+    value: 20
+  },
+  {
+    label: '10:15',
+    value: 21
+  },
+  {
+    label: '10:30',
+    value: 22
+  },
+  {
+    label: '10:45',
+    value: 23
+  },
+  {
+    label: '11:00',
+    value: 24
+  },
+  {
+    label: '11:15',
+    value: 25
+  },
+  {
+    label: '11:30',
+    value: 26
+  },
+  {
+    label: '11:45',
+    value: 27
+  },
+  {
+    label: '12:00',
+    value: 28
+  },
+  {
+    label: '12:15',
+    value: 29
+  },
+  {
+    label: '12:30',
+    value: 30
+  },
+  {
+    label: '12:45',
+    value: 31
+  },
+  {
+    label: '13:00',
+    value: 32
+  },
+  {
+    label: '13:15',
+    value: 33
+  },
+  {
+    label: '13:30',
+    value: 34
+  },
+  {
+    label: '13:45',
+    value: 35
+  },
+  {
+    label: '14:00',
+    value: 36
+  },
+  {
+    label: '14:15',
+    value: 37
+  },
+  {
+    label: '14:30',
+    value: 38
+  },
+  {
+    label: '14:45',
+    value: 39
+  },
+  {
+    label: '15:00',
+    value: 40
+  },
+  {
+    label: '15:15',
+    value: 41
+  },
+  {
+    label: '15:30',
+    value: 42
+  },
+  {
+    label: '15:45',
+    value: 43
+  },
+  {
+    label: '16:00',
+    value: 44
+  },
+  {
+    label: '16:15',
+    value: 45
+  },
+  {
+    label: '16:30',
+    value: 46
+  },
+  {
+    label: '16:45',
+    value: 47
+  },
+  {
+    label: '17:00',
+    value: 48
+  },
+  {
+    label: '17:15',
+    value: 49
+  },
+  {
+    label: '17:30',
+    value: 50
+  },
+  {
+    label: '17:45',
+    value: 51
+  },
+  {
+    label: '18:00',
+    value: 52
+  },
+  {
+    label: '18:15',
+    value: 53
+  },
+  {
+    label: '18:30',
+    value: 54
+  },
+  {
+    label: '19:00',
+    value: 55
+  },
+  {
+    label: '19:15',
+    value: 56
+  },
+  {
+    label: '19:30',
+    value: 57
+  },
+  {
+    label: '19:45',
+    value: 58
+  },
+  {
+    label: '20:00',
+    value: 59
+  },
+  {
+    label: '20:15',
+    value: 60
+  },
+  {
+    label: '20:30',
+    value: 61
+  },
+  {
+    label: '20:45',
+    value: 62
+  },
+  {
+    label: '21:00',
+    value: 63
+  },
+  {
+    label: '21:15',
+    value: 64
+  },
+  {
+    label: '21:30',
+    value: 65
+  },
+  {
+    label: '21:45',
+    value: 66
+  },
+  {
+    label: '22:00',
+    value: 67
+  },
+  {
+    label: '22:15',
+    value: 68
+  },
+  {
+    label: '22:30',
+    value: 69
+  },
+  {
+    label: '22:45',
+    value: 70
+  },
+  {
+    label: '23:00',
+    value: 71
+  },
+  {
+    label: '23:15',
+    value: 72
+  },
+  {
+    label: '23:30',
+    value: 73
+  },
+  {
+    label: '23:45',
+    value: 74
+  },
+  {
+    label: '00:00',
+    value: 75
+  },
+  {
+    label: '00:15',
+    value: 76
+  },
+  {
+    label: '00:30',
+    value: 77
+  },
+  {
+    label: '00:45',
+    value: 78
+  },
+  {
+    label: '01:00',
+    value: 79
+  },
+  {
+    label: '01:15',
+    value: 80
+  },
+  {
+    label: '01:30',
+    value: 81
+  },
+  {
+    label: '01:45',
+    value: 82
+  },
+  {
+    label: '02:00',
+    value: 83
+  },
+  {
+    label: '02:15',
+    value: 84
+  },
+  {
+    label: '02:30',
+    value: 85
+  },
+  {
+    label: '02:45',
+    value: 86
+  },
+  {
+    label: '03:00',
+    value: 87
+  },
+  {
+    label: '03:15',
+    value: 88
+  },
+  {
+    label: '03:30',
+    value: 89
+  },
+  {
+    label: '03:45',
+    value: 90
+  },
+  {
+    label: '04:00',
+    value: 91
+  },
+  {
     label: '04:15',
-    value: 4.25
+    value: 92
   },
   {
     label: '04:30',
-    value: 4.5
+    value: 93
   },
   {
     label: '04:45',
-    value: 4.75
+    value: 94
   }
 ];
+
+export const labels = {};
